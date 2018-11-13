72a9a57ee6658f4a9137a6e1a572da1766e832bb
Mohamed lahri <mokalahri@gmail.com>
2018-11-13 16:29:06 +0100

    creation fichier quete POO2

---
+++ Hangar.java
@@ -0,0 +1,10 @@
+public class Hangar {
+
+	public static void main(String args[]){
+		Car Citroen = new Car("Citroen", 40);
+		Boat Flotte = new Boat("Flotte", 50);
+		System.out.println(Citroen.doStuff());
+		System.out.println(Flotte.doStuff());
+	}
+}
+		
